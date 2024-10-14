aa26e2289df4aa5277d37930dcbc9cf2e4194cf3
Sercan Sahin <s.sahin@iat.rwth-aachen.de>
2024-10-14 10:50:29 +0200

    fix nginx configuration

--- stop-supervisor.sh
+++ stop-supervisor.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-printf "READY\n";
+printf "READY\n"
 
 while read line; do
-  echo "Processing Event: $line" >&2;
+  echo "Processing Event: $line" >&2
   kill $PPID
 done < /dev/stdin
